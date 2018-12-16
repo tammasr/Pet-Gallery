8aa536fca8251f03faaac4ee57ee526c4396808e
SravyaTamma <Sravya.Tamma@ge.com>
2018-12-16 14:08:16 -0500

    lazy load images on scroll & resize event listeners

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
 import '../styles/index.scss';
-
-import '../styles/index.scss';
 import petService from './petService';
+import lazyload from './lazyLoadImages';
+
 
 function init () {
     petService.fetchPets();
+    document.addEventListener("scroll", lazyload.loadImages);
+    window.addEventListener("resize", lazyload.loadImages);
+    window.addEventListener("load", lazyload.loadImages);
+    window.addEventListener("orientationChange", lazyload.loadImages);
 }
 init();
---
+++ src/scripts/lazyLoadImages.js
@@ -0,0 +1,27 @@
+var lazyload = {
+    loadImages : function () {
+        let lazyloadThrottleTimeout;
+        let lazyloadImages = document.querySelectorAll("img.lazy");
+
+        if (lazyloadImages.length) {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+            }
+
+            lazyloadThrottleTimeout = setTimeout(function () {
+                let scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function (img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        if ('src' in img.dataset) {
+                            img.src = img.dataset.src;
+                            img.classList.remove('lazy');
+                        }
+                    }
+                });
+            }, 20);
+        }
+    }
+
+};
+
+export default lazyload;
